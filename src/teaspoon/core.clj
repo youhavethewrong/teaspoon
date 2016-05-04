6bce455d4ce3d018f368f564dd7d73294768e65a
ESC <youhavethewrong@gmail.com>
2016-05-04 12:24:33 -0500

    Start modelling problem space.

--- src/teaspoon/core.clj
+++ src/teaspoon/core.clj
@@ -1,6 +1,15 @@
 (ns teaspoon.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol ICity
+  (get-x [c])
+  (get-y [c])
+  (distance-to [c c1]))
+
+(defrecord City [x y]
+  ICity
+  (get-x [c] x)
+  (get-y [c] y)
+  (distance-to [c c1]
+    (let [x-dist (Math/abs (- (get-x c) (get-x c1)))
+          y-dist (Math/abs (- (get-y c) (get-y c1)))]
+      (Math/sqrt (+ (* x-dist x-dist) (* y-dist y-dist))))))
--- test/teaspoon/core_test.clj
+++ test/teaspoon/core_test.clj
@@ -1,7 +1,15 @@
 (ns teaspoon.core-test
   (:require [clojure.test :refer :all]
-            [teaspoon.core :refer :all]))
+            [teaspoon.core :refer :all])
+  (:import [teaspoon.core City]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest city-test
+  (testing "Tests of the ICity interface."
+    (let [c (City. 10 12)
+          c1 (City. 11 13)]
+      (is (= 10 (get-x c)))
+      (is (= 12 (get-y c)))
+      (is (= 11 (get-x c1)))
+      (is (= 13 (get-y c1)))
+      (is (= (Math/sqrt 2.0) (distance-to c c1)))
+      )))
