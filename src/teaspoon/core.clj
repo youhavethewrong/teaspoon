2fe73180f9ceaeec2af3ae24197619fc3fd6cc3f
ESC <youhavethewrong@gmail.com>
2016-05-05 07:00:30 -0500

    Implement ITourManager.

--- src/teaspoon/core.clj
+++ src/teaspoon/core.clj
@@ -13,3 +13,15 @@
     (let [x-dist (Math/abs (- (get-x c) (get-x c1)))
           y-dist (Math/abs (- (get-y c) (get-y c1)))]
       (Math/sqrt (+ (* x-dist x-dist) (* y-dist y-dist))))))
+
+
+(defprotocol ITourManager
+  (add-city [t c])
+  (get-city [t i])
+  (number-of-cities [t]))
+
+(defrecord TourManager [l]
+  ITourManager
+  (add-city [t c] (TourManager. (conj l c)))
+  (get-city [t i] (nth l i))
+  (number-of-cities [t] (count l)))
--- test/teaspoon/core_test.clj
+++ test/teaspoon/core_test.clj
@@ -1,7 +1,7 @@
 (ns teaspoon.core-test
   (:require [clojure.test :refer :all]
             [teaspoon.core :refer :all])
-  (:import [teaspoon.core City]))
+  (:import [teaspoon.core City TourManager]))
 
 (deftest city-test
   (testing "Tests of the ICity interface."
@@ -12,4 +12,17 @@
       (is (= 11 (get-x c1)))
       (is (= 13 (get-y c1)))
       (is (= (Math/sqrt 2.0) (distance-to c c1)))
+      )))
+
+(deftest tour-manager-test
+  (testing "Tests of the ITourManager interface."
+    (let [t (TourManager. [])
+          c (City. 10 12)
+          c1 (City. 11 13)
+          u (add-city t c)]
+      (is (= 0 (number-of-cities t)))
+
+      (is (= 1 (number-of-cities u)))
+      (is (= 10 (get-x (get-city u 0))))
+      (is (= 12 (get-y (get-city u 0))))
       )))
